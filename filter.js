d828bb3eb638554725880d5d1d2f59cdb268be02
PierreSarciat <phsarciat@mailo.com>
2025-08-06 20:44:14 +0200

    récupération des categoryId avec addEventListener et regroupement des fichier JS

--- filter.js
+++ filter.js
@@ -1,10 +1,10 @@
 fetch("http://localhost:5678/api/categories")
     .then(res => res.json())
     .then(data => {
-        let display = '<li><button class="bouton">Tous</button></li>';
+        let display = '<li><button class="bouton" data-id="0">Tous</button></li>';
         for (let filtre of data) {
             display += `            
-        <li><button class="bouton">${filtre.name}</button></li>`
+        <li><button class="bouton" data-id="${filtre.id}">${filtre.name}</button></li>`
         }
 
         document.querySelector(".filtre ul").innerHTML = display;
@@ -14,17 +14,41 @@ fetch("http://localhost:5678/api/categories")
         boutons.forEach(bouton => {
 
             bouton.addEventListener("click", () => {
+                const categoryId = bouton.dataset.id;
                 console.log("Bouton trier ok");
+                console.log(categoryId);
+
 
             });
         });
     })
 
 
-            .catch(err => console.log(err));
+    .catch(err => console.log(err));
+
+
+    fetch("http://localhost:5678/api/works")
+    .then(res => res.json())
+    .then(data => {
+        let display = "";
+        for (let figure of data) {
+
+            display += ` 
+        <figure>
+        <img src="${figure.imageUrl}" alt="${figure.title}">
+			<figcaption>${figure.title
+                }</figcaption>
+</figure>
+   `;
+        }
+
+        console.log(display)
+        document.querySelector(".gallery").innerHTML = display;
+    })
+
+    .catch(err => console.log(err));
 
 
 
-    
 
 

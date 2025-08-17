d4ab59735d6323ac572a0c46efaf9383601f3d7d
PierreSarciat <phsarciat@mailo.com>
2025-08-17 11:36:10 +0200

    gestion ouverture et fermeture des boites modales

--- boiteModale.js
+++ boiteModale.js
@@ -5,6 +5,13 @@ const modal = document.getElementById("myModal")
 const openBtn = document.getElementById("openModal")
 const closeBtn = document.getElementById("closeModal")
 
+const modal2 = document.getElementById("myModal2")
+const openModal2 = document.getElementById("ajouterPhoto")
+const closeBtn2 = document.getElementById("closeModal2")
+const returnModal = document.getElementById("returnModal")
+
+
+
 // Ouvrir
 openBtn.addEventListener("click", () => {
   modal.style.display = "flex"
@@ -24,3 +31,48 @@ modal.addEventListener("click", (e) => {
     modal.setAttribute("aria-hidden", "true")
   }
 })
+
+//        boite modale 2
+
+
+// ouvrir boîte modale2
+
+openModal2.addEventListener("click", () => {
+  modal2.style.display = "flex"
+  modal2.setAttribute("aria-hidden", "false")
+  modal.style.display = "none"
+})
+
+//  fermer en cliquant sur x
+
+closeBtn2.addEventListener("click", () => {
+  modal2.style.display = "none"
+  modal2.setAttribute("aria-hidden", "true")
+
+})
+
+// fermer en cliquant à l' exterieur
+
+modal2.addEventListener("click", (e) => {
+  if (e.target === modal2) {
+    modal2.style.display = "none"
+    modal2.setAttribute("aria-hidden", "true")
+  }
+})
+
+
+//  retour boîte modale début
+
+returnModal.addEventListener("click", () => {
+  modal2.style.display = "none"
+  modal2.setAttribute("aria-hidden", "true")
+  modal.style.display = "flex"
+  modal.setAttribute("aria-hidden", "false")
+})
+
+
+
+
+
+
+
